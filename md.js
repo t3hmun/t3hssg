4f7f7097c235d008d4836d2c9d0b03325ae0eb33
t3hmun <git@t3hmun.com>
2021-01-17 20:37:46 +0000

    Simple md parsing.

---
+++ md.js
@@ -0,0 +1,38 @@
+const MarkdownIt = require("markdown-it");
+const hljs = require("highlight.js");
+
+const defaultMdIt = configureMarkdownIt();
+
+function configureMarkdownIt() {
+  // Mostly copy paste from the MarkdownIt readme, customised.
+  const mdit = new MarkdownIt({
+    html: true, // Enable HTML tags in source
+    xhtmlOut: false, // Use '/' to close single tags (<br />).
+    // This is only for full CommonMark compatibility.
+    breaks: false, // Convert '\n' in paragraphs into <br>
+    langPrefix: "language-", // CSS language prefix for fenced blocks. Can be
+    // useful for external highlighters.
+    linkify: false, // Autoconvert URL-like text to links
+
+    // I do not want typographer, do not want quotes interfered with.
+    typographer: false,
+
+    highlight: function (str, lang) {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(lang, str).value;
+        } catch (err) {
+          throw err;
+        }
+      }
+
+      return ""; // use external default escaping
+    },
+  });
+  return mdit;
+}
+
+module.exports.convertToHtml = (md) => {
+  const html = defaultMdIt.render(md);
+  return html;
+};
