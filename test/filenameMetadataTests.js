ea122972fef7ccc9d974e6f7aeb768574d7eab5d
t3hmun <git@t3hmun.com>
2021-01-23 11:58:16 +0000

    Some more filename parse tests.

--- test/filenameMetadataTests.js
+++ test/filenameMetadataTests.js
@@ -10,6 +10,31 @@ describe(`filenameMetadata extractMetadataFromFilename`, function () {
       filename: "2020-10-15-Hello.md",
       expected: { title: "Hello", date: new Date(2020, 10, 15) },
     },
+    {
+      description: "no time one letter title",
+      filename: "2020-10-15-H.md",
+      expected: { title: "H", date: new Date(2020, 10, 15) },
+    },
+    {
+      description: "no time two words dash title",
+      filename: "2020-10-15-Hello-Something.md",
+      expected: { title: "Hello-Something", date: new Date(2020, 10, 15) },
+    },
+    {
+      description: "no time title with dots",
+      filename: "2020-10-15-Hello.test.md",
+      expected: { title: "Hello.test", date: new Date(2020, 10, 15) },
+    },
+    {
+      description: "with morning time one word title",
+      filename: "2020-10-15-0306-Hello.md",
+      expected: { title: "Hello", date: new Date(2020, 10, 15, 03, 06) },
+    },
+    {
+      description: "with afternoon time one word title",
+      filename: "2020-10-15-1606-Hello.md",
+      expected: { title: "Hello", date: new Date(2020, 10, 15, 16, 06) },
+    },
   ];
 
   tests.forEach(function (test) {
