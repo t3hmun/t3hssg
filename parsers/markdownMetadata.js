ffea31dd4aba8446d4a798bcb7418ed2da5a1f50
t3hmun <git@t3hmun.com>
2021-01-24 10:37:51 +0000

    Soem tests fro h1 and description parser, fix parser.

--- parsers/markdownMetadata.js
+++ parsers/markdownMetadata.js
@@ -1,7 +1,7 @@
 /* Find metadata from within the markdown.  */
 
 const h1Regex = /((^.*?\r?\n)|(^))# (?<h1>.+?)\r?\n/gs;
-const descirptionWithH1Regex = /^.*?\r?\n# (.+?)\r?\n(?<description>.+?)\r?\n## /gs;
+const descirptionWithH1Regex = /((^.*?\r?\n)|(^))# (.+?)\r?\n(?<description>.+?)\r?\n## /gs;
 const descriptionNoH1 = new RegExp(/(?<description>.+?)\r?\n## /gs);
 
 /**
@@ -13,7 +13,7 @@ module.exports.extractMarkdownMetadata = (markdown) => {
   const h1Result = h1Regex.exec(markdown);
   const h1 = h1Result?.groups?.h1 ?? null;
   let description;
-  if (h1 == null) {
+  if (h1 !== null) {
     descirptionWithH1Regex.lastIndex = 0;
     const result = descirptionWithH1Regex.exec(markdown);
     description = result?.groups?.description ?? null;
--- test/markdownMetadataTests.js
+++ test/markdownMetadataTests.js
@@ -3,10 +3,10 @@ const should = chai.should;
 
 const markdownMetadata = require("../parsers/markdownMetadata.js");
 
-describe(`markdownMetadata extractMarkdownMetadata`, function () {
+describe(`markdownMetadata extractMarkdownMetadata H1 only`, function () {
   const tests = [
     {
-      description: "One word H1 only",
+      description: "One word H1",
       markdown: "# Test\n\ncontent",
       expected: {
         title: "Test",
@@ -14,7 +14,7 @@ describe(`markdownMetadata extractMarkdownMetadata`, function () {
       },
     },
     {
-      description: "One word H1 only with crlf after",
+      description: "One word H1 with crlf after",
       markdown: "# Test\r\n\r\ncontent",
       expected: {
         title: "Test",
@@ -22,7 +22,7 @@ describe(`markdownMetadata extractMarkdownMetadata`, function () {
       },
     },
     {
-      description: "One word H1 only with crlf before",
+      description: "One word H1 with crlf before",
       markdown: "\r\n# Test\n\ncontent",
       expected: {
         title: "Test",
@@ -30,7 +30,7 @@ describe(`markdownMetadata extractMarkdownMetadata`, function () {
       },
     },
     {
-      description: "One word H1 only with crlf before and after",
+      description: "One word H1 with crlf before and after",
       markdown: "\r\n# Test\r\n\r\ncontent",
       expected: {
         title: "Test",
@@ -38,7 +38,7 @@ describe(`markdownMetadata extractMarkdownMetadata`, function () {
       },
     },
     {
-      description: "Two word H1 only",
+      description: "Two word H1",
       markdown: "# Test Header\n\n content",
       expected: {
         title: "Test Header",
@@ -46,11 +46,82 @@ describe(`markdownMetadata extractMarkdownMetadata`, function () {
       },
     },
     {
-      description: "H1 only with text before",
+      description: "H1 with text before",
       markdown: "\n> Some comment about stuff\n\n# Test Header\n\n content",
       expected: {
         title: "Test Header",
         description: null,
+      },
+    },
+  ];
+
+  tests.forEach((test) => {
+    it(`parse ${test.description}`, function () {
+      const actual = markdownMetadata.extractMarkdownMetadata(test.markdown);
+      actual.should.eql(test.expected);
+    });
+  });
+});
+
+describe(`markdownMetadata extractMarkdownMetadata H1 and description`, function () {
+  const tests = [
+    {
+      description: "One word H1 and description",
+      markdown: "# Test\n\ncontent\n\n## h2",
+      expected: {
+        title: "Test",
+        description: "\ncontent\n",
+      },
+    },
+    {
+      description: "One word H1 and description extra newlines",
+      markdown: "# Test\n\ncontent\n\n\n\n## h2",
+      expected: {
+        title: "Test",
+        description: "\ncontent\n\n\n",
+      },
+    },
+    {
+      description: "One word H1 and description with crlfs",
+      markdown: "# Test\r\n\r\ncontent\r\n\r\n## h2",
+      expected: {
+        title: "Test",
+        description: "\r\ncontent\r\n",
+      },
+    },
+    {
+      description: "One word H1 and description and 2 h2s",
+      markdown:
+        "# Test\n\ncontent\n\n## h2\nMore stuff\n\n## h2\n\neven more content",
+      expected: {
+        title: "Test",
+        description: "\ncontent\n",
+      },
+    },
+    {
+      description: "One word H1 and description and 2 h2s with crlfs",
+      markdown:
+        "# Test\r\n\r\ncontent\r\n\r\n## h2\r\nMore stuff\r\n\r\n## h2\r\n\r\neven more content",
+      expected: {
+        title: "Test",
+        description: "\r\ncontent\r\n",
+      },
+    },
+    {
+      description: "Two word H1 and content",
+      markdown: "# Test Header\n\n some content\n\n## h2",
+      expected: {
+        title: "Test Header",
+        description: "\n some content\n",
+      },
+    },
+    {
+      description: "H1 and content with text before",
+      markdown:
+        "\n> Some comment about stuff\n\n# Test Header\n\n content\n\n## h2",
+      expected: {
+        title: "Test Header",
+        description: "\n content\n",
       },
     },
   ];
