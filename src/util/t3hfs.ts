3c473e70cbaa884d48970633222bbf63cd276357
t3hmun <git@t3hmun.com>
2021-02-07 21:31:19 +0000

    qip Ts converts t3hfs,  it realllly needs tests.

--- src/util/t3hfs.ts
+++ src/util/t3hfs.ts
@@ -1,16 +1,16 @@
-const fs = require("fs");
-const path = require("path");
+import * as fs from "fs";
+import * as path from "path";
 
-module.exports.readAllFilesInDir = async (dir) => {
+export async function readAllFilesInDir(dir: string): Promise<File[]> {
   const infos = await statDir(dir);
   const files = await readAllFiles(infos);
-  return infos;
-};
+  return files;
+}
 
 /**
  * Runs fs.stat on every item in dir returning {dir, name, path, stats}[].
  */
-function statDir(dir, encoding = "utf-8") {
+function statDir(dir: string, encoding: BufferEncoding = "utf-8"): Promise<Info[]> {
   return new Promise((resolve, reject) => {
     fs.readdir(dir, encoding, (err, listing) => {
       if (err) reject(err);
@@ -23,15 +23,21 @@ function statDir(dir, encoding = "utf-8") {
  * Reads all files (skips any items that are dirs)
  * @param {infos[]} infos - An info is a {dir, name, path, stats}, may be a file or dir.
  */
-function readAllFiles(infos) {
-  return new Promise((resolve, reject) => {
+function readAllFiles(infos: Info[]): Promise<File[]> {
+  return new Promise<File[]>((resolve, reject) => {
     fsreadfileAll(infos, resolve, reject);
   });
 }
 
-function fsreadfileAll(infos, resolve, reject, i = 0, files = []) {
+function fsreadfileAll(
+  infos: Info[],
+  resolve: (files: File[]) => void,
+  reject: (err: NodeJS.ErrnoException) => void,
+  i = 0,
+  files: File[] = []
+): void {
   const currentItem = infos[i];
-  if (currentItem.isFile) {
+  if (currentItem.stats.isFile()) {
     fs.readFile(currentItem.path, "utf-8", (err, data) => {
       if (err) reject(err);
       else files.push({ ...currentItem, data });
@@ -61,13 +67,20 @@ function fsreadfileAll(infos, resolve, reject, i = 0, files = []) {
  * @param {number} i - Position in listing for next iteration.
  * @param {[]} infos - Array with results, {dir, name, path, stats}, stats beign the fs.stat result.
  */
-function fsstatAll(dir, listing, resolve, reject, i = 0, infos = []) {
+function fsstatAll(
+  dir: string,
+  listing: string[],
+  resolve: (info: Info[]) => void,
+  reject: (err: NodeJS.ErrnoException) => void,
+  i = 0,
+  infos: Info[] = []
+): void {
   const fileName = listing[i]; // We don't know if this is a file yet.
   const filePath = path.join(dir, fileName);
 
   fs.stat(filePath, (err, stats) => {
     if (err) reject(err);
-    else if (stats.isFile) {
+    else if (stats.isFile()) {
       const info = {
         dir: dir,
         name: fileName,
@@ -83,4 +96,15 @@ function fsstatAll(dir, listing, resolve, reject, i = 0, infos = []) {
       resolve(infos);
     }
   });
+}
+
+interface Info {
+  dir: string;
+  name: string;
+  path: string;
+  stats: fs.Stats;
+}
+
+interface File extends Info {
+  data: string;
 }
